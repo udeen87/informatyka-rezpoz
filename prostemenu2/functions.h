746f2dda6bad427fe0defc48b695a97fb0cd67cc
udeen87 <260564@edu.p.lodz.pl>
2026-06-22 16:01:36 +0200

    zajecia

---
+++ prostemenu2/functions.h
@@ -0,0 +1,13 @@
+#ifndef FUNCTIONS_H
+#define FUNCTIONS_H
+
+#define SIZE 10
+
+void printArray(int array[], int size);
+void menu(void);
+int findMinimum(int array[], int size);
+int findMaximum(int array[], int size);
+float calculateAverage(int array[], int size);
+void enterValues(int array[], int size);
+
+#endif
